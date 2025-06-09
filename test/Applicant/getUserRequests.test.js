cac12fc02aa137ec2b3a93e34d55fe342244a12d
paydelimon22 <valentina2212.gonzalez@gmail.com>
2025-06-09 12:30:31 -0600

    test: get user requests

---
+++ test/Applicant/getUserRequests.test.js
@@ -0,0 +1,53 @@
+import * as chai from 'chai';
+const { expect, should } = chai;
+import { request } from 'chai-http';
+
+import chaiHttp from 'chai-http';
+import app from '../../index.js'
+
+chai.use(chaiHttp);
+should();
+
+describe('/api/applicant/get-user-requests/1', function () {
+
+  it('Should return 200', function (done) {
+
+    request.execute(app)
+      .get('/api/applicant/get-user-requests/1')
+      .end((err, res) => {
+        res.should.have.status(200);
+      })
+
+    done();
+  });
+
+  it('Should include the correct params', function (done) {
+
+    request.execute(app)
+      .get('/api/applicant/get-user-requests/1')
+      .end((err, res) => {
+        res.body.forEach(request => {
+          expect(request).to.be.an('object');
+          expect(request).to.have.property('request_id');
+          expect(request).to.have.property('destination_country');
+          expect(request).to.have.property('beginning_date');
+          expect(request).to.have.property('ending_date');
+          expect(request).to.have.property('status');
+        });
+      })
+
+    done();
+  });
+
+  it('Should return err 404', function (done) {
+
+    request.execute(app)
+      .get('/api/applicant/get-user-requests/999999')
+      .end((err, res) => {
+        res.should.have.status(404);
+      })
+
+    done();
+  });
+
+});
